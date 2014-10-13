d080ddbcae5a04f303e3b8b260a342c213c16a52
Philip Yannick Benischke <phbenisc@htwg-konstanz.de>
2014-10-13 17:21:29 +0100

    Package Javadoc for new Packages

---
+++ sortingMeasure/src/main/java/de/htwg_konstanz/ui/configuration/distribution/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * DistributionControllers to manage the Distributions as part of the RandomProblemGenerator
+ */
+/**
+ * @author Philip Benischke
+ *
+ */
+package de.htwg_konstanz.ui.configuration.distribution;
