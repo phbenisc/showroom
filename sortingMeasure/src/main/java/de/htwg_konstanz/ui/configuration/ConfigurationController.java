2b49dca1ad9d20a78a47aeede5ea4d35a161307d
Philip Yannick Benischke <phbenisc@htwg-konstanz.de>
2014-10-13 20:37:08 +0100

    altered import

--- sortingMeasure/src/main/java/de/htwg_konstanz/ui/configuration/ConfigurationController.java
+++ sortingMeasure/src/main/java/de/htwg_konstanz/ui/configuration/ConfigurationController.java
@@ -30,8 +30,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import uk.napierdevsoc.sortingAlgorithms.SortingManager;
-import de.htwg_konstanz.sortingMeasure.ControlerAndWindowFactory;
 import de.htwg_konstanz.ui.configuration.problemGenerator.ProblemGeneratorManager;
+import de.htwg_konstanz.ui.main.ControlerAndWindowFactory;
 import de.htwg_konstanz.ui.main.IController;
 import de.htwg_konstanz.ui.result.ResultController;
 
