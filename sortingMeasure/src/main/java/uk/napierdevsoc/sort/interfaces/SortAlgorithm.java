4dbdc693484732bcda42a1b2a276e72fea99a706
Philip Yannick Benischke <phbenisc@htwg-konstanz.de>
2015-05-01 20:37:37 +0100

    added Comment

--- sortingMeasure/src/main/java/uk/napierdevsoc/sort/interfaces/SortAlgorithm.java
+++ sortingMeasure/src/main/java/uk/napierdevsoc/sort/interfaces/SortAlgorithm.java
@@ -24,12 +24,22 @@ public abstract class SortAlgorithm {
 	public ISortData sortWithComparisonAndShiftMeasure(double[] array){
 		return sortWithComparisonAndShiftMeasure(array, new SortData());
 	}
+	/**
+	 * sort an double array
+	 * @param array to Sort. the order of the array will be altered.
+	 * @param sortData to profile the efficiency of the used algorithm
+	 * @return the some sortData Object witch was inserted but filled with data
+	 */
 	public abstract ISortData sortWithComparisonAndShiftMeasure(double[] array, ISortData sortData);
 	
 	public ISortData sortWithMeasure(double[] array){
+		//get Stopwatch
 		StopWatch stopWatch = new Slf4JStopWatch(logger, Slf4JStopWatch.TRACE_LEVEL);
+		//sort 
 		ISortData data = sortWithComparisonAndShiftMeasure(array);
+		//stop 
 		stopWatch.stop();
+		//add time to data
 		data.setTime(stopWatch.getElapsedTime());
 		return data;
 	}
