2106658344c3becb6a86d78d938698f18bb86fd7
Philip Yannick Benischke <phbenisc@htwg-konstanz.de>
2015-05-02 22:44:56 +0100

    added comments and test

--- sortingMeasure/src/main/java/de/htwg_konstanz/sort/util/SortUtil.java
+++ sortingMeasure/src/main/java/de/htwg_konstanz/sort/util/SortUtil.java
@@ -4,22 +4,33 @@ import de.htwg_konstanz.sort.interfaces.ISortData;
 
 
 public class SortUtil {
-	
+	/**
+	 * private constructor to avoid instantiation of the class (static methods only)
+	 */
 	private SortUtil(){}
 	
-	
+	/**
+	 * swaps two elements in a array and adds three shifts to the sortData
+	 * @param array the array were the elements are located
+	 * @param first the index of the first element
+	 * @param secound the index of the second element
+	 */
 	public static void swap(double[] array, int first, int secound){
 		synchronized (array) {
 			double temp;
 	        temp = array[first]; 
             array[first] = array[secound]; 
             array[secound] = temp;
-			
-		}
-		
+		}		
 	}
 
-
+	/**
+	 * swaps two elements in a array and adds three shifts to the sortData
+	 * @param array the array were the elements are located
+	 * @param first the index of the first element
+	 * @param secound the index of the second element
+	 * @param sortData the sortData element 
+	 */
 	public static void swap(double[] array, int first, int secound, ISortData sortData) {
 		sortData.addShifts(3);
 		swap(array, first, secound);		
