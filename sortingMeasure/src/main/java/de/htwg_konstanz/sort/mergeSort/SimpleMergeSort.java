a42a407b7580e2c5601704ec6b5b13b429a09582
Philip Yannick Benischke <phbenisc@htwg-konstanz.de>
2015-05-02 20:32:32 +0100

    added Logger

--- sortingMeasure/src/main/java/de/htwg_konstanz/sort/mergeSort/SimpleMergeSort.java
+++ sortingMeasure/src/main/java/de/htwg_konstanz/sort/mergeSort/SimpleMergeSort.java
@@ -1,14 +1,28 @@
 package de.htwg_konstanz.sort.mergeSort;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.htwg_konstanz.sort.interfaces.ISortData;
 import de.htwg_konstanz.sort.interfaces.SortAlgorithm;
 
+/** 
+ * @author Philip Yannick Benischke
+ * @author Carnegie Mellon University, School of COmputer Science
+ */
 public class SimpleMergeSort extends SortAlgorithm {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMergeSort.class);
 
 	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see de.htwg_konstanz.sort.interfaces.SortAlgorithm#sortWithComparisonAndShiftMeasure(double[], de.htwg_konstanz.sort.interfaces.ISortData)
+	 */
 	public ISortData sortWithComparisonAndShiftMeasure(double[] array, ISortData sortData) {
 		double[] tmp = new double[array.length];
 		mergeSort(array, tmp,  0,  array.length - 1, sortData);
+		LOGGER.debug("sortWithComparisonAndShiftMeasure(...) -> {}",sortData);
 		return sortData;
 	}
 
@@ -27,12 +41,13 @@ public class SimpleMergeSort extends SortAlgorithm {
         int k = left;
         int num = rightEnd - left + 1;
 
-        while(left <= leftEnd && right <= rightEnd)
+        while(left <= leftEnd && right <= rightEnd){
             if(array[left] <= array[right])
                 tmp[k++] = array[left++];
             else
                 tmp[k++] = array[right++];
         	sortData.addComparison();
+        }
         while(left <= leftEnd){    // Copy rest of first half
             tmp[k++] = array[left++];
             sortData.addShift();
@@ -47,5 +62,4 @@ public class SimpleMergeSort extends SortAlgorithm {
             sortData.addShift();
         }
 	}
-
 }
